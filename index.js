acc6d0f2ec31ffe45ae33bb43b96d221e79a7c2d
jolly <vasanth_r@outlook.com>
2023-03-12 13:30:29 +0530

    Uses lodash.debounce to prevent .on('add') triggering multiple times. * Now it is set to trigger beyond an inter of 180 micro seconds. * Adds lodash.debounce npm package.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 #!/usr/bin/env node
+// Require lodash to solve problem caused by on('add') problem
+const debounce = require('lodash.debounce');
 // Require in chokidar
 const chokidar = require('chokidar');
+// Custom function for handling on('add')
+const start = debounce(() => {
+    console.log('Starting User\'s Program');
+}, 180);
 // One-liner for current directory
 chokidar.watch('.')
-    .on('add', () => console.log('File Added'))
+    .on('add', start)
     .on('change', () => console.log('File Changed'))
     .on('unlink', () => console.log('File Unlinked'));
