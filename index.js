f2dab7d4f45fbc3cfc81d6ebcfaf17a284f736d4
jolly <vasanth_r@outlook.com>
2023-03-12 16:28:48 +0530

    Adds functionality to alert User of absence of the file provided. * Uses fn.promises.access(). * Changes index.js.

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 // Adding Caporal
 const program = require('caporal');
+// Adding File System
+const fs = require('fs');
 // Caporal code specific to our project
 program.version('0.1.0')
     .argument('[file-name]', 'Name of the file to execute')
-    .action(({ fileName }) => {
+    .action(async ({ fileName }) => {
         // If there is no fileName given by user, use index.js
         const name = fileName || 'index.js';
+        // Access from FS as a promise (checks existence of file)
+        // Will throw error when file absent, handle with try{}catch(err)
+        try {
+            await fs.promises.access(name);
+        } catch (error) {
+            throw new Error(`Could not find the file "${name}"`);
+        }
         // Custom function for handling on('add')
         const start = debounce(() => {
             console.log("Starting User's Program");
